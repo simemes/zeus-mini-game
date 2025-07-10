c18b65d03d3b75feb6a03a69c7cef0ec958cebc3
jerrylin <jerrlin@simemes.xyz>
2025-07-10 23:27:27 +0800

    update loadPage

--- src/components/LoadPage.vue
+++ src/components/LoadPage.vue
@@ -1,16 +1,34 @@
 <template>
   <img src="/images/simemes_bg.png" class="absolute top-0 left-0 w-full h-full object-cover -z-10 pointer-events-none">
   <div class="w-full h-full flex flex-col justify-center items-center">
-    <img src="/images/zeus_drop_logo.png" class="flex-5 w-[60%] object-contain">
 
+    <!-- pass & chance -->
+    <div class="flex flex-1 justify-between">
+      <div class="relative w-[12%] m-5 bg-[#00000050] rounded-full p-2 aspect-square flex justify-center items-center">
+        <img src="/images/zeus_pass.png" class="rotate-[-15deg]">
+      </div>
+      <div class="relative w-[18%] h-[35%] m-5 bg-[#00000050] rounded-full p-2 flex justify-center items-center">
+        <div class="flex-1">
+          <img src="/images/zeus_3chance.png" class="rotate-[-15deg]">
+        </div>
+        <div class="absolute w-[20%] bottom-[5%] left-[30%]">
+          <img src="/images/plus_icon.png">
+        </div>
+        <div class="flex-2 text-[14px] font-[700]">{{ $store.users_profile.maxPlayCount - $store.users_profile.todayPlayCount }}</div>
+      </div>
+    </div>
+    <!-- zeus_drop_logo -->
+    <img src="/images/zeus_drop_logo.png" class="flex-4 w-[60%] object-contain">
+
+    <!-- Loading -->
     <div v-if="!$store.isLoaded" class="flex-1 flex load-text font-[Inter,sans-serif] font-bold text-[20px] [text-shadow:1px_1px_0_#000,-1px_-1px_0_#000,1px_-1px_0_#000,-1px_1px_0_#000]">
       <p>Loading</p>
       <p v-if="load_point >= 1">.</p>
       <p v-if="load_point >= 2">.</p>
       <p v-if="load_point >= 3">.</p>
     </div>
-
-    <div v-if="$store.isLoaded" class="flex flex-col justify-start flex-5 mx-[15%] load-text font-[Inter,sans-serif] font-bold text-[12px]">
+    <!-- Ready Zone -->
+    <div v-if="$store.isLoaded" class="flex flex-col justify-start flex-4 mx-[15%] load-text font-[Inter,sans-serif] font-bold text-[12px]">
       <div>
         <div class="my-5">Collect items, avoid bombs, score big, and climb the leaderboard!</div>
         <div class="my-5">But beware, as time ticks on, the challenge accelerates.</div>
